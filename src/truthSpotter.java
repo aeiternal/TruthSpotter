30e33abee497c11a87e9ebfd8ea489f97a0bb326
Cade Schurz <cschurz@tilsontech.com>
2020-03-08 14:16:06 -0400

    key pair addition

--- src/truthSpotter.java
+++ src/truthSpotter.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class truthSpotter {
     
     public  int numPlayers;
+    private char[][] play_key_pairs = {{'q','s'},{'x','c'},{'n','m'},{'l','p'}};//key pairs for yes and no the left key for yes and the right key for no
     
     public void truthSpotter(int numP) {
-        
+        numPlayers = numP;
     }
     
     public static void initializePlayers(int x) {
@@ -26,6 +27,7 @@ public static void main(String[] args){
        
         int numP = scr.nextInt();
         
+        //while loop to check for the correct number of players
         while(true) {
             if (numP < 2) {
                System.out.println("at least 2 players are required for play");
