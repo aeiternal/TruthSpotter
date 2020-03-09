135b0bc3218c27340d333aec9af3c83f9ca38f39
BrendenPittiglio <61477648+BrendenPittiglio@users.noreply.github.com>
2020-03-08 22:41:50 -0500

    Add files via upload

--- bin/truthSpotter.java
+++ bin/truthSpotter.java
@@ -4,21 +4,18 @@ import java.awt.event.KeyEvent;
 
 public class truthSpotter {
 
+	// Values for adding points to the players
 	static int answerPoints = 100;
 	static int bonusPoints = 75;
-	
-	
-	//Creates an arrayList to store our string values for questions and answers
+
+	// Creates an arrayList to store our string values for questions and answers
 	private ArrayList<String[]> QA_pairs = new ArrayList<String[]>();
-	
-	//Creates an arrayList to store our characters 
+
+	// Creates an arrayList to store our characters
 	public ArrayList<playerCharacters> players = new ArrayList<playerCharacters>();
 
-	private char[][] play_key_pairs = { { 'q', 's' }, { 'x', 'c' }, { 'n', 'm' }, { 'l', 'p' } };// key pairs for yes
-																									// and no the left
-																									// key for yes and
-																									// the right key for
-																									// no
+	// Key pairs to be assigned to the players, left is true and right is false
+	private char[][] play_key_pairs = { { 'q', 's' }, { 'x', 'c' }, { 'n', 'm' }, { 'l', 'p' } };
 
 	public truthSpotter(int numPlayers, ArrayList<Scanner> catfiles) {
 		// initializes everything in the game
@@ -28,25 +25,25 @@ public class truthSpotter {
 
 		// Creates the playerCharacters based on number entered
 		for (int i = 0; i < numPlayers; i++) {
-			System.out.println("Please enter player " + (i+1) + "'s name");
+			System.out.println("Please enter player " + (i + 1) + "'s name");
 			String name = scr.next();
 			players.add(new playerCharacters(name, play_key_pairs[i][0], play_key_pairs[i][1], 0));
 		}
-		// the following searches through the catagory files
+		// the following searches through the category files
 		// and is intended to add a string array to the QA_pairs list with index 0 the
 		// question and index 1 the answer
 		String prevLine;
 		String currLine = null;
-		
+
 		for (Scanner f : catfiles) {
 			while (f.hasNext()) {
 				prevLine = currLine;
 				currLine = f.nextLine();
-				
-				if (currLine.contains("true")|| currLine.contains("false")){
+
+				if (currLine.contains("true") || currLine.contains("false")) {
 					QA_pairs.add(new String[] { prevLine, currLine });
 				}
-				
+
 			}
 		}
 
@@ -58,18 +55,18 @@ public class truthSpotter {
 		// key is found in the string and is correct points are added to your total
 		Scanner scr = new Scanner(System.in);
 		Random rand = new Random();
-		
-		//Gets a question number from the randomizer
+
+		// Gets a question number from the randomizer
 		int questionNum = rand.nextInt(QA_pairs.size());
-		
-		//Gets a Q/A pair based on the random rolled
+
+		// Gets a Q/A pair based on the random rolled
 		String[] QA = QA_pairs.get(questionNum);
-		
-		
-		
+
 		System.out.println(QA[0] + "\n \n Press Enter when everyone has entered their answer.");
+
+		// Returns the input from players answering true or false
 		String answers = scr.next();
-		
+
 		int points = answerPoints;
 		System.out.println("The answer is: " + QA[1]);
 		for (int i = 0; i < answers.length(); i++) {
@@ -90,12 +87,17 @@ public class truthSpotter {
 
 			}
 		}
-		System.out.println("Current scores are:");
+
+		// Prints all the player scores
+		System.out.println("\nCurrent scores are:");
 		for (playerCharacters player : players) {
-			System.out.println(player.getName()  + ": " +  player.getScore());
+			System.out.println(player.getName() + ": " + player.getScore());
 		}
+
+		// Removes the question so it cant be asked again
 		QA_pairs.remove(questionNum);
-		
+
+		// Makes the transition prettier (in my opinion)
 		System.out.println("\n\nThe next question is: ");
 	}
 
@@ -103,9 +105,10 @@ public class truthSpotter {
 
 		String fn = null;
 
-		ArrayList<Scanner> rawCatagories = new ArrayList<Scanner>();
+		ArrayList<Scanner> rawCategories = new ArrayList<Scanner>();
 
-		// loops through the command line args where each input is a question catagory
+		// loops through the command line arguments where each input is a question
+		// category
 		for (int i = 0; i < args.length; i++) {
 			if (args.length == 0) {
 				System.out.println("The name of the input data file" + " should be on the command line.\nTerminating.");
@@ -113,7 +116,7 @@ public class truthSpotter {
 			} else {
 				try {
 					fn = args[i];
-					rawCatagories.add(new Scanner(new File(fn)));
+					rawCategories.add(new Scanner(new File(fn)));
 				} catch (Exception e) {
 					System.out.println("Could not open the file " + fn + '.');
 					return;
@@ -135,15 +138,19 @@ public class truthSpotter {
 			} else if (numP > 4) {
 				System.out.println("No more than 4 players are allowed to play.");
 				numP = scr.nextInt();
-			} 
-			else {
+			} else {
 				break;
 			}
 		}
 
-		truthSpotter game = new truthSpotter(numP, rawCatagories);// if the number of players is correct a new instance
-																	// of the truthSpotter Class is made called game
+		// If we get an appropriate number of players we create an instance of
+		// truthSpotter called game
+		truthSpotter game = new truthSpotter(numP, rawCategories);
 
+		//Just a print out to increase visual clarity
+		System.out.println("\n\n\nThe first question is:");
+		
+		//Calls for questions to be asked
 		while (true) {
 			game.ask_question();
 		}
